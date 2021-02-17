c91eef2b1a515992b04b54d3c4f356e2948c324f
AndyIsraelYanez <camilo.casas200@gmail.com>
2021-02-17 15:37:16 -0500

    actualizacion

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgComponentOutletComponent } from './components/ng-component-outlet/ng-component-outlet.component';
 import { DinamicComponentComponent } from './components/dinamic-component/dinamic-component.component';
+import { NgMostradorComponent } from './components/ng-mostrador/ng-mostrador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgComponentOutletComponent,
-    DinamicComponentComponent
+    DinamicComponentComponent,
+    NgMostradorComponent
   ],
   imports: [
     BrowserModule
