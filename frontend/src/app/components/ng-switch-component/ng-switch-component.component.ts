3deca71cb9a363fbf73e6e2ef5353e449e02f801
JaviPtz <jam.pastaz@yavirac.deu.ec>
2021-02-17 15:51:46 -0500

    generando el componente para ngSwitch

---
+++ frontend/src/app/components/ng-switch-component/ng-switch-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-switch-component',
+  templateUrl: './ng-switch-component.component.html',
+  styleUrls: ['./ng-switch-component.component.scss']
+})
+export class NgSwitchComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
