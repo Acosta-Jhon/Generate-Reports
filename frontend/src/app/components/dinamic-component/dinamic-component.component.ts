59e854e57e442bf587627105d2660ac188ddc43a
Acosta-Jhon <jhonerick2018@gmail.com>
2021-02-17 19:48:38 -0500

    design

--- frontend/src/app/components/dinamic-component/dinamic-component.component.ts
+++ frontend/src/app/components/dinamic-component/dinamic-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-dinamic-component',
   templateUrl: './dinamic-component.component.html',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicComponentComponent implements OnInit {
 
-  constructor() { }
+  public arr: Array<any> = [
+    { framework: 'Angular', img: 'http://assets.stickpng.com/images/5847ea22cef1014c0b5e4833.png' },
+    { framework: 'React JS', img: 'https://iconape.com/wp-content/files/zk/93042/svg/react.svg' },
+    { framework: 'Vue JS', img: 'http://assets.stickpng.com/images/58482acecef1014c0b5e4a1e.png' }
+  ]
 
   ngOnInit(): void {
+  
   }
 
 }
