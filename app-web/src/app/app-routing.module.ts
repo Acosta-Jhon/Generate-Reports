3e947f609175f056ef4a8dcaca06588d1a486d9e
Acosta-Jhon <jhonerick2018@gmail.com>
2021-02-27 17:09:17 -0500

    parameters

--- app-web/src/app/app-routing.module.ts
+++ app-web/src/app/app-routing.module.ts
@@ -4,18 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CreateRolesComponent } from './parameters/roles/create-roles/create-roles.component';
 import { ListRolesComponent } from './parameters/roles/list-roles/list-roles.component';
-import { CreateUsersComponent } from './parameters/users/create-users/create-users.component';
-import { ListUsersComponent } from './parameters/users/list-users/list-users.component';
+import { CreateUsersComponent } from './process/users/create-users/create-users.component';
+import { ListUsersComponent } from './process/users/list-users/list-users.component';
 import { CreateAdressesComponent } from './parameters/addresses/create-addresses/create-addresses.component';
 import { ListAddressesComponent } from './parameters/addresses/list-addresses/list-addresses.component';
 import { CreateCitiesComponent } from './parameters/cities/create-cities/create-cities.component';
 import { ListCitiesComponent } from './parameters/cities/list-cities/list-cities.component';
-
-
-
-
-
-
+import { ListQuestionsComponent } from './parameters/questions/list-questions/list-questions.component';
+import { CreateQuestionsComponent } from './parameters/questions/create-questions/create-questions.component';
+import { ListJobsComponent } from './parameters/jobs/list-jobs/list-jobs.component';
+import { CreateJobsComponent } from './parameters/jobs/create-jobs/create-jobs.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -32,12 +30,18 @@ const routes: Routes = [
     { path: 'addresses/list-addresses', component: ListAddressesComponent },
     { path: 'addresses/create-address', component: CreateAdressesComponent },
     { path: 'addresses/edit-address/:id', component: CreateAdressesComponent },
-
     //cities
     { path: 'cities/list-cities', component: ListCitiesComponent },
     { path: 'cities/create-cities', component: CreateCitiesComponent },
     { path: 'cities/edit-cities/:id', component: CreateCitiesComponent },
-
+    //jobs
+    { path: 'jobs/list-jobs', component: ListJobsComponent },
+    { path: 'jobs/create-job', component: CreateJobsComponent },
+    { path: 'jobs/edit-job/:id', component: CreateJobsComponent },
+    //questions
+    { path: 'questions/list-questions', component: ListQuestionsComponent },
+    { path: 'questions/create-question', component: CreateQuestionsComponent },
+    { path: 'questions/edit-question/:id', component: CreateQuestionsComponent },
 
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
--- app-web/src/app/app.module.ts
+++ app-web/src/app/app.module.ts
@@ -10,16 +10,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { ListUsersComponent } from './parameters/users/list-users/list-users.component';
-import { CreateUsersComponent } from './parameters/users/create-users/create-users.component';
+import { ListUsersComponent } from './process/users/list-users/list-users.component';
+import { CreateUsersComponent } from './process/users/create-users/create-users.component';
 import { CreateRolesComponent } from './parameters/roles/create-roles/create-roles.component';
 import { ListRolesComponent } from './parameters/roles/list-roles/list-roles.component';
 import { ListAddressesComponent } from './parameters/addresses/list-addresses/list-addresses.component';
 import { CreateAdressesComponent } from './parameters/addresses/create-addresses/create-addresses.component';
 import { CreateCitiesComponent } from './parameters/cities/create-cities/create-cities.component';
 import { ListCitiesComponent } from './parameters/cities/list-cities/list-cities.component';
-
-
+import { CreateJobsComponent } from './parameters/jobs/create-jobs/create-jobs.component';
+import { ListJobsComponent } from './parameters/jobs/list-jobs/list-jobs.component';
+import { CreateQuestionsComponent } from './parameters/questions/create-questions/create-questions.component';
+import { ListQuestionsComponent } from './parameters/questions/list-questions/list-questions.component';
 
 @NgModule({
   declarations: [
@@ -32,8 +34,11 @@ import { ListCitiesComponent } from './parameters/cities/list-cities/list-cities
     ListAddressesComponent,
     CreateAdressesComponent,
     CreateCitiesComponent,
-    ListCitiesComponent
-  
+    ListCitiesComponent,
+    CreateJobsComponent,
+    ListJobsComponent,
+    CreateQuestionsComponent,
+    ListQuestionsComponent,  
   ],
   imports: [
     BrowserModule,
